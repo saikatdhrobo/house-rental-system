0d58012cdf38b7399b8819549b98c348964a4837
MD. Kowsar Islam <137004941+Kowsar21@users.noreply.github.com>
2024-12-28 22:48:30 +0600

    SignIn.jsx

--- SignIn.jsx
+++ SignIn.jsx
@@ -58,7 +58,7 @@ const SignIn = () => {
           <input type="email" required placeholder='Email' onChange={(e) => setEmail(e.target.value)} />
           <input type="password" required placeholder='Password' onChange={(e) => setPassword(e.target.value)} />
           <input type="submit" value='Sign In' className={styles.submitButton} />
-          <p>Forgot Password?</p>
+          <p>Forgot Password??</p>
         </form>
       </div>
     </>

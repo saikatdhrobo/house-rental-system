92b6c2002217ab7a6295ddfda90b90e4364396ea
Jakaria Saikat Dhrobo <113936334+saikatdhrobo@users.noreply.github.com>
2026-05-13 22:47:23 +0600

    Update models.py

--- models.py
+++ models.py
@@ -3,23 +3,23 @@ from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 
 
 class Users(Base):
-    __tablename__ = 'users'
+    __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    email = Column(String, unique=True)
-    username = Column(String, unique=True)
+    email = Column(String, unique=True, nullable=False)
+    username = Column(String, unique=True, nullable=False)
     first_name = Column(String)
     last_name = Column(String)
-    hashed_password = Column(String)
+    hashed_password = Column(String, nullable=False)
     is_active = Column(Boolean, default=True)
     role = Column(String)
 
 
 class Todos(Base):
-    __tablename__ = 'todos'
+    __tablename__ = "todos"
 
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(String)
+    title = Column(String, nullable=False)
     description = Column(String)
     priority = Column(Integer)
     complete = Column(Boolean, default=False)
